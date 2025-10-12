fe128353a6ec369dfce904bfc7970884991dadc7
HFely <hfurbanof@gmail.com>
2025-10-12 17:47:50 -0500

    Add blueprints for articulos, entidades_relaciones, reportes, usuarios, and vales_almacen

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,20 @@
 # app/routes/__init__.py
 from .main import main_bp
 from .auth import auth_bp
+from .articulo import articulo_bp
+from .entidad_relacion import entidad_relacion_bp
+from .reportes import reportes_bp
+from .usuario import usuario_bp
+from .vale_almacen import vale_almacen_bp
 
 def register_blueprints(app):
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(articulo_bp, url_prefix='/articulos')
+    app.register_blueprint(entidad_relacion_bp, url_prefix='/entidades_relaciones')
+    app.register_blueprint(reportes_bp, url_prefix='/reportes')
+    app.register_blueprint(usuario_bp, url_prefix='/usuarios')
+    app.register_blueprint(vale_almacen_bp, url_prefix='/vales_almacen')
 
 # Registrar namespaces de la API
 from app.routes.auth import auth_ns
